ebf5ad48b1c8e15f602cbccb529e1ecdcbd20ecf
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2024-12-18 23:08:39 +0600

    Refactor Navbar component to include collapsible submenus and enhance styling with new CSS classes

--- app/components/common/Navbar.tsx
+++ app/components/common/Navbar.tsx
@@ -1,17 +1,51 @@
 "use client";
 
-import { Facebook, Home, Info, Instagram, Mail, Twitter } from "lucide-react";
+import { ChevronDownIcon, Facebook, Instagram, Twitter } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-const links = [
-  { href: "/", label: "Home", icon: Home },
-  { href: "/about", label: "About", icon: Info },
-  { href: "/contact", label: "Contact", icon: Mail },
+interface LinkItem {
+  id: number;
+  name: string;
+  link: string;
+  sublinks?: { name: string; link: string }[];
+}
+
+// Navigation links
+const links: LinkItem[] = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    link: "/about",
+    sublinks: [
+      { name: "Our Story", link: "/about/story" },
+      { name: "Team", link: "/about/team" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Services",
+    link: "/services",
+    sublinks: [
+      { name: "Service 1", link: "/services/service-1" },
+      { name: "Service 2", link: "/services/service-2" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Contact",
+    link: "/contact",
+  },
 ];
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
   useEffect(() => {
     if (open) {
@@ -23,69 +57,115 @@ export default function Navbar() {
 
   const closeDrawer = () => setOpen(false);
 
+  function toggleMenu(index: number): void {
+    setActiveIndex(activeIndex === index ? null : index);
+  }
+
   return (
-    <nav className="bg-base-300 p-3">
+    <nav className="px-4 py-4 top-0 left-0 w-full z-30">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-lg">
-          <Link href="/" className="text-2xl font-bold tracking-wide">
-            Viriditas
-          </Link>
-        </div>
-        <div className="hidden md:flex space-x-4">
+        <Link href="/" className="text-2xl font-bold tracking-wide">
+          Viriditas
+        </Link>
+
+        <div className="hidden md:flex space-x-10">
           {links.map((link) => (
-            <Link key={link.href} href={link.href} className="">
-              {link.label}
-            </Link>
+            <div key={link.id} className="relative group">
+              <Link href={link.link} className="flex items-center space-x-4">
+                <span className="font-medium link-underline link-underline-black">
+                  {link.name}
+                </span>
+              </Link>
+
+              {link.sublinks && (
+                <div className="absolute top-full left-0 px-6 bg-base-300 hidden group-hover:block rounded-sm">
+                  {link.sublinks.map((sublink, index) => (
+                    <Link
+                      key={index}
+                      href={sublink.link}
+                      className="block p-2 hover:text-primary text-nowrap sublink-hover"
+                    >
+                      {sublink.name}
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
           ))}
         </div>
-        <div className="md:hidden">
-          <button className="focus:outline-none" onClick={() => setOpen(!open)}>
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              ></path>
-            </svg>
+
+        <div className={`md:hidden z-50 ${open ? "z-40 text-white" : ""}`}>
+          <button
+            className={`focus:outline-none menu-button ${open ? "open" : ""}`}
+            onClick={() => setOpen(!open)}
+          >
+            <div className="line line1"></div>
+            <div className="line line2"></div>
+            <div className="line line3"></div>
           </button>
         </div>
       </div>
+
       {open && (
         <div
-          className="fixed inset-0 bg-opacity-50 z-40"
+          className="fixed inset-0 bg-black bg-opacity-50"
           onClick={closeDrawer}
         ></div>
       )}
+
       <div
-        className={`fixed top-0 left-0 h-full transition-transform transform ${
+        className={`fixed top-0 left-0 h-full transition-transform duration-500 ease-in-out transform ${
           open ? "translate-x-0" : "-translate-x-full"
         } md:hidden z-50`}
         style={{ width: "270px" }}
       >
         <div className="bg-base-200 flex flex-col h-full">
-          <div className="p-3 border-b border-base-200 bg-base-300 text-2xl font-bold tracking-wide">
+          <div className="p-3 border-b border-base-200 bg-base-300 text-2xl font-bold tracking-wide text-center">
             Viriditas
           </div>
-          <div className="flex-1 overflow-y-auto">
-            {links.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
-                className="p-4 flex items-center"
-                onClick={closeDrawer}
-              >
-                <link.icon className="mr-2" />
-                {link.label}
-              </Link>
+
+          {/* colapsibe manu  */}
+          <div className="p-6 h-full overflow-y-auto">
+            {links.map((link, index) => (
+              <div key={link.id} className="mb-4">
+                <div
+                  className="flex justify-between items-center cursor-pointer"
+                  onClick={() => toggleMenu(index)}
+                >
+                  <span className="font-semibold">{link.name}</span>
+                  {link.sublinks && (
+                    <span
+                      className={`transition-transform duration-500 ease-in-out ${
+                        activeIndex === index ? "rotate-180" : ""
+                      }`}
+                    >
+                      <ChevronDownIcon className="w-5 h-5" />
+                    </span>
+                  )}
+                </div>
+                {link.sublinks && (
+                  <div
+                    className={`ml-4 mt-2 overflow-hidden transition-all duration-500 ease-in-out ${
+                      activeIndex === index
+                        ? "max-h-96 opacity-100"
+                        : "max-h-0 opacity-0"
+                    }`}
+                  >
+                    {link.sublinks.map((sublink, subIndex) => (
+                      <Link
+                        key={subIndex}
+                        href={sublink.link}
+                        className="block p-2 font-medium hover:text-primary text-nowrap"
+                      >
+                        {sublink.name}
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </div>
             ))}
           </div>
+
           <div className="p-3 border-t border-base-200 bg-base-300 flex justify-around">
             <Link
               href="https://facebook.com"
